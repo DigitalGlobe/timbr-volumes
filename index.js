92bb065a359c5f622032d7c65b7521ddc6e8f27e
glortho <jed@dataverse.me>
2016-09-07 13:25:29 -0400

    add bodyparser

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const exec = require( 'child_process' ).exec;
 const koa = require('koa');
+const bodyParser = require( 'koa-bodyparser' );
 
 const host = '10.0.0.10';
 const port = 8000;
@@ -20,8 +21,6 @@ const respond = ctx => [
   }
 ];
 
-const app = koa();
-
 const handlers = {
   '/': {
     GET: function list() {
@@ -29,15 +28,19 @@ const handlers = {
         .then( ...respond( this ) );
     },
     POST: function create() {
-      return $exec( `sudo zfs create -o quota=${this.size} -o sharenfs=on juno-pool/${this.name}` )
+      const body = this.request.body;
+      return $exec( `sudo zfs create -o quota=${body.size} -o sharenfs=on juno-pool/${body.name}` )
         .then( ...respond( this ) );
     }
   }
 };
 
-app.use(function *() {
-  const handler = ( handlers[this.url] || {} )[this.method];
-  if ( handler ) yield handler.call( this );
-});
+const app = koa();
 
-app.listen( port, host, () => console.log( `Listening on ${host}:${port}` ) );
+app
+  .use( bodyParser() )
+  .use(function *() {
+    const handler = ( handlers[this.url] || {} )[this.method];
+    if ( handler ) yield handler.call( this );
+  })
+  .listen( port, host, () => console.log( `Listening on ${host}:${port}` ) );
